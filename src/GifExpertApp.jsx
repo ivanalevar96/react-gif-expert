961af16cda5313c0682f1515c43778ee00e13cd4
Ivan <ivan.alevar96@gmail.com>
2024-05-06 01:59:29 -0400

    avance cap 80

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,28 @@
-
+import { useState } from 'react';
+import { AddCategory } from './Components/AddCategory';
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball', 'Samurai X']);
+
+  const onAddCategory = () => {
+    setCategories([...categories, 'HunterXHunter']);
+  };
   return (
     <>
+      {/* Titulo */}
       <h2>GifExpertApp</h2>
+
+      {/* Input */}
+      <AddCategory setCategories={setCategories} />
+
+      {/* Listado de Gifs */}
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{category}</li>
+        })}
+      </ol>
+
+      {/* Gif Item */}
     </>
   );
 }
